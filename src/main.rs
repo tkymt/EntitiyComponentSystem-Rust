02368044a570af9f96d837ef51a62e6368c27e8d
Taku Yamamoto <jts0ymt@outlook.jp>
2023-05-01 20:05:41 +0900

    不要になった関数を消した。

--- src/main.rs
+++ src/main.rs
@@ -40,37 +40,6 @@ fn main() {
     }
 }
 
-// idを増やして、配列に要素を追加する
-fn create_entity(id: &mut usize, ary: &mut Vec<Position>){
-    *id += 1;
-    ary.push(Position { x: 0, y: 0 });
-}
-
-// idを増やして、配列に指定された値の要素を追加する
-fn create_entity_pos(id: &mut usize, ary: &mut Vec<Position>, data: Position) {
-    *id += 1;
-    ary.push(data);
-}
-
-// 配列の指定されたidの要素に、指定された値を代入する
-fn set_data (id: &usize, ary: &mut Vec<Position>, data: Position) {
-    ary[*id] = data;
-}
-
-fn create_entity_vel_zero_init(id: &mut usize, ary: &mut Vec<Velocity>) {
-    *id += 1;
-    ary.push(Velocity { x: 0, y: 0 });
-}
-
-fn create_entity_vel(id: &mut usize, ary: &mut Vec<Velocity>, data: Velocity) {
-    *id += 1;
-    ary.push(data);
-}
-
-fn set_data_vel(id: &usize, ary: &mut Vec<Velocity>, data: Velocity) {
-    ary[*id] = data;
-}
-
 // ジェネリックなデータ型を引数に取る create_entity 関数
 fn generic_create_entity <T> (id: &mut usize, v: &mut Vec<T>, data: T) {
     v.push(data);
